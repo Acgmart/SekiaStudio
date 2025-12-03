cacd13948930b83af2fc744725b3bd5d514345f1
咲夜詩 <44355206+Acgmart@users.noreply.github.com>
2025-12-04 00:03:52 +0800

    迁移资源包demores

--- ET/Packages/cn.etetet.statesync/Scripts/HotfixView/Client/Scene/SceneChangeStart_AddComponent.cs
+++ ET/Packages/cn.etetet.statesync/Scripts/HotfixView/Client/Scene/SceneChangeStart_AddComponent.cs
@@ -13,9 +13,10 @@ namespace ET.Client
                 Scene currentScene = root.CurrentScene();
 
                 ResourcesLoaderComponent resourcesLoaderComponent = currentScene.GetComponent<ResourcesLoaderComponent>();
-            
+
                 // 加载场景资源
-                await resourcesLoaderComponent.LoadSceneAsync($"Packages/cn.etetet.demores/Scenes/{currentScene.Name}.unity", LoadSceneMode.Single);
+                string assetName = $"Assets/Scenes/{currentScene.Name}.unity";
+                await resourcesLoaderComponent.LoadSceneAsync(assetName, LoadSceneMode.Single);
                 // 切换到map场景
 
                 //await SceneManager.LoadSceneAsync(currentScene.Name);
