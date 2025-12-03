df66477cfe13e5d110af8f0e5804e32b0796a9ff
咲夜詩 <44355206+Acgmart@users.noreply.github.com>
2025-12-03 14:58:06 +0800

    去除Router

--- ET/Packages/cn.etetet.login/Scripts/Hotfix/Client/NetClient/Main2NetClient_LoginHandler.cs
+++ ET/Packages/cn.etetet.login/Scripts/Hotfix/Client/NetClient/Main2NetClient_LoginHandler.cs
@@ -11,26 +11,24 @@ namespace ET.Client
         {
             string account = request.Account;
             string password = request.Password;
-            // 创建一个ETModel层的Session
-            root.RemoveComponent<RouterAddressComponent>();
-            // 获取路由跟realmDispatcher地址
-            RouterAddressComponent routerAddressComponent =
-                    root.AddComponent<RouterAddressComponent, string>(request.Address);
-            await routerAddressComponent.Init();
+
+            int portIndex = request.Address.LastIndexOf(":");
+            IPAddress ipAddress = IPAddress.Parse(request.Address.Substring(0, portIndex));
+            int port = int.Parse(request.Address.Substring(portIndex + 1));
+            IPEndPoint realmAddress = new IPEndPoint(ipAddress, port);
+            AddressFamily addressFamily = ipAddress.AddressFamily;
+
 #if UNITY_WEBGL
-            root.AddComponent<NetComponent, IKcpTransport>(new WebSocketTransport(routerAddressComponent.AddressFamily));
+            root.AddComponent<NetComponent, IKcpTransport>(new WebSocketTransport(addressFamily));
 #else
-            root.AddComponent<NetComponent, IKcpTransport>(new UdpTransport(routerAddressComponent.AddressFamily));
+            root.AddComponent<NetComponent, IKcpTransport>(new UdpTransport(addressFamily));
 #endif
             root.GetComponent<FiberParentComponent>().ParentFiberId = request.OwnerFiberId;
 
             NetComponent netComponent = root.GetComponent<NetComponent>();
             
-            IPEndPoint realmAddress = routerAddressComponent.GetRealmAddress(account);
-
-
             R2C_Login r2CLogin;
-            using (Session session = await netComponent.CreateRouterSession(realmAddress, account, password))
+            using (Session session = netComponent.Create(realmAddress))
             {
                 C2R_Login c2RLogin = C2R_Login.Create();
                 c2RLogin.Account = account;
@@ -39,7 +37,7 @@ namespace ET.Client
             }
 
             // 创建一个gate Session,并且保存到SessionComponent中
-            Session gateSession = await netComponent.CreateRouterSession(NetworkHelper.ToIPEndPoint(r2CLogin.Address), account, password);
+            Session gateSession = netComponent.Create(NetworkHelper.ToIPEndPoint(r2CLogin.Address));
             gateSession.AddComponent<ClientSessionErrorComponent>();
             root.AddComponent<SessionComponent>().Session = gateSession;
             C2G_LoginGate c2GLoginGate = C2G_LoginGate.Create();
--- ET/Packages/cn.etetet.login/Scripts/Model/Share/SceneType.cs
+++ ET/Packages/cn.etetet.login/Scripts/Model/Share/SceneType.cs
@@ -4,7 +4,5 @@
 	{
 		public const int Realm = PackageType.Login * 1000 + 1;
 		public const int Gate = PackageType.Login * 1000 + 2;
-		public const int Router = PackageType.Login * 1000 + 3;
-		public const int RouterManager = PackageType.Login * 1000 + 4;
 	}
 }
